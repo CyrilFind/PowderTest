9f2c0276464ca8c900f2129b082834eb42d4a4b2
cyrilfind <cyrilfind@gmail.com>
2020-09-26 16:30:22 +0200

    improve UI/UX

--- app/src/main/java/com/powder/powdertest/MainActivity.kt
+++ app/src/main/java/com/powder/powdertest/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.powder.powdertest
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 
 val urls = listOf(
@@ -17,6 +22,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewPager2_mainActivity.adapter = VideoAdapter(this, videos)
+        val videoAdapter = VideoAdapter(this, videos)
+        viewPager2_mainActivity.adapter = videoAdapter
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
+
 }
--- app/src/main/java/com/powder/powdertest/VideoFragment.kt
+++ app/src/main/java/com/powder/powdertest/VideoFragment.kt
@@ -4,6 +4,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -12,15 +14,24 @@ import kotlinx.android.synthetic.main.fragment_video.*
 
 class VideoFragment : Fragment(R.layout.fragment_video) {
     lateinit var video: Video
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private val exoPlayer: ExoPlayer by lazy {
         val userAgent = Util.getUserAgent(requireContext(), requireContext().packageName)
         val dataSourceFactory = DefaultDataSourceFactory(requireContext(), userAgent)
         val uri = Uri.parse(video.url)
         val source = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
-        val exoPlayer = SimpleExoPlayer.Builder(requireContext()).build().apply { prepare(source) }
+        SimpleExoPlayer.Builder(requireContext()).build().apply { prepare(source) }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
         playerView.player = exoPlayer
     }
+
+    override fun onPause() {
+        super.onPause()
+        exoPlayer.playWhenReady = false
+    }
+
     fun getInstance(video: Video): Fragment = VideoFragment().apply { this.video = video }
 }
